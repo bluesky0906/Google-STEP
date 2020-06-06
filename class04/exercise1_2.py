37dae1bcbd9effedba3fe24e114e1789694c6606
Sora Tagami <loslonbervene.1923@icloud.com>
2020-06-06 22:20:28 +0900

    add class04/exercise1_2

---
+++ class04/exercise1_2.py
@@ -0,0 +1,28 @@
+from exercise1_1 import search_bfs, read_file
+
+if __name__ == '__main__':
+    user_dic = read_file()
+    length = len(user_dic)
+
+    not_connecting = []
+    results = []
+    for id, user in user_dic.items():
+        for id2, user2 in user_dic.items():
+            # 同じ人の時
+            if id == id2:
+                continue
+            result = search_bfs(user_dic, id, id2)
+            # つながっていない組み合わせが見つかったら
+            if not result:
+                not_connecting.append('{} -> {}'.format(user.name, user2.name))
+            results.append(result)
+
+    # つながっている確率とつながっていない確率
+    false = results.count(False)
+    true = results.count(True)
+    total = len(results)
+    print(f'connecting: {true/total * 100} %')
+    print(f'not connecting: {false/total * 100} %')
+
+    # つながっていない組み合わせ
+    print('Not connecting list: ', not_connecting)
